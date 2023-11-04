e826c9ee861b145c0aaf6961f902d4f514fe2374
Rodrigo Gomez <rodrigogp4812@gmail.com>
2023-11-03 21:50:58 -0600

    '03/11/2023_RGP'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuComponent } from './menu/menu/menu.component';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
+
   imports: [
-    BrowserModule
+    BrowserModule,
+    // FormsModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
